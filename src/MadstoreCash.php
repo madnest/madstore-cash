dcf13283e810668afecd5aefae26d3d1b76b270e
Jakub Theimer <theimer@madne.st>
2020-06-22 13:53:54 +0200

    add createPayment method

--- src/MadstoreCash.php
+++ src/MadstoreCash.php
@@ -1,28 +1,24 @@
 <?php
 
-// namespace Madnest\MadstoreCash;
+namespace Madnest\MadstoreCash;
 
-// class MadstoreGopay implements PaymentOption
-// {
-//     /**
-//      * Creates standard GoPay payment
-//      *
-//      * @param Purchasable $purchasable
-//      * @param <array|array> $params Additional params
-//      * @param <array|array> $options Additional options
-//      * @return PaymentResponse
-//      */
-//     public function createPayment(Purchasable $purchasable, array $params = [], array $options = []): PaymentResponse
-//     {
-//         $response = $this->gopay->createPayment($this->getParams($purchasable, $params, $options));
+use Madnest\Madstore\Payment\Contracts\PaymentOption;
+use Madnest\Madstore\Payment\Contracts\Purchasable;
+use Madnest\Madstore\Payment\Enums\PaymentStatus;
+use Madnest\Madstore\Payment\PaymentResponse;
 
-//         return (new PaymentResponse($response->statusCode, $response->json['state']))
-//             ->setOrderNumber($response->json['order_number'])
-//             ->setAmount($response->json['amount'])
-//             ->setCurrency($response->json['currency'])
-//             ->setPayer($response->json['payer'])
-//             ->setRedirectUrl($response->json['gw_url'])
-//             ->setRedirect($response->json['gw_url'] ? true : false)
-//             ->setErrors([]);
-//     }
+class MadstoreCash implements PaymentOption
+{
+    /**
+     * Creates standard cash payment
+     *
+     * @param Purchasable $purchasable
+     * @param <array|array> $params Additional params
+     * @param <array|array> $options Additional options
+     * @return PaymentResponse
+     */
+    public function createPayment(Purchasable $purchasable, array $params = [], array $options = []): PaymentResponse
+    {
+        return (new PaymentResponse(200, PaymentStatus::CREATED))->setRedirect(false)->setErrors([]);
+    }
 }
